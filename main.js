1e52120f6fe78d08bfe253329eba5ffc972e8120
ELATIFI AMINA <aminaelatifi06@gmail.com>
2022-08-26 21:35:01 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function validation(){
+
+var number = document.getElementById("number").value;
+
+var errormsg = document.getElementById("errormsg");
+var text;
+
+/*
+if (number.length !== 10) {
+    text = "Entrer un numéro valide";
+    errormsg.innerHTML = text;
+    return false;
+}
+*/
+
+
+  alert("Message envoyé !");
+  return true;
+}
